4373df28cdbf042a954be3c091dd7a97624ad7e3
Zhangcx19 <105340504+Zhangcx19@users.noreply.github.com>
2022-05-11 14:07:27 +0800

    Create BO.py

---
+++ BO.py
@@ -0,0 +1,18 @@
+import numpy as np
+from robo.fmin import bayesian_optimization
+from search import bo_train_imagenet
+import logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
+
+# Define the objective_function.
+def objective_function(policies, time):
+    acc = bo_train_imagenet.main(policies, time)
+    error = 1 - acc
+    return error
+
+# Define the bounds and dimensions of the input space.
+lower = np.array([0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0])
+upper = np.array([195.99999,1,9,1,9, 195.99999,1,9,1,9, 195.99999,1,9,1,9])
+
+# Start Bayesian optimization to optimize the objective function
+results = bayesian_optimization(objective_function, lower, upper, num_iterations=80, n_init=10, i=10)
